090e0240d7f9569a0bc81e2523dd841644305456
p24jmorris <144147868+JonMorris8@users.noreply.github.com>
2023-09-13 09:09:20 -0400

    finished lab 2-2

--- file2.py
+++ file2.py
@@ -1,13 +1,15 @@
 # 1) Create a Python file named lab_2-2.py
 
 # 2) Set a equal to 3 and b equal to a
-
+a = 3
+b = a
 # 3) Print the value of b and the value of a.
-
+print (a,b)
 # 4) What is the value of each variable and why? (Answer as a comment)
-
+#a and b are equal to 3 becuse we first set a equal to 3 and then set b equal to a makeing them both the same.
 # 5) Set a equal to 7.
-
+a = 7
 # 6) Print the value of a and the value of b
-
+print (a,b)
 # 7) What is the value of each variable and why? (Answer as a comment)
+#a is 7 and b is 3 becuse we first set a equal to 3 and then set b equal to a makeing b 3 then we redefine a as 7 not affecting b.
