56adbdacd4dfff109da6cf962060b17b1c26af81
p24jmorris <144147868+JonMorris8@users.noreply.github.com>
2023-09-13 09:05:09 -0400

    finished assinment
    ez

--- file1.py
+++ file1.py
@@ -1,9 +1,24 @@
 #Create a python file named lab_2-1.py
 #Input the following into the Python interpreter and see what the output is. Be mindful of spaces. Each expression needs to be visible in your final code.
-"""
-123 + 321 - 321
-20+ 202020 - 420 + 20
-2 * 4 * 6 * 8		9 / 5		9 // 5
-13/0	3 % 10	3 % 10
-2 ** 3		2 * 2 * 2		0/0
-"""
+
+print(123 + 321 - 321)
+#123
+print(20+ 202020 - 420 + 20)
+#201640
+print(2 * 4 * 6 * 8)
+#384
+print(9 / 5)
+#1.8
+print(9 // 5)
+#1
+#print(13/0)
+#Error
+print(3 % 10)
+#3
+print(2 ** 3)
+#8
+print(2 * 2 * 2)
+#8
+#print(0/0)
+#Error
+print("good")
